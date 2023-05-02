6d380080b772badfb5176891fb1da08c06fa61b5
Lynchpin61 <131472200+Lynchpin61@users.noreply.github.com>
2023-05-02 16:22:47 +0800

    Add files via upload

--- OPE-app2.0/frontend/src/app/components/signup/signup.component.ts
+++ OPE-app2.0/frontend/src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -12,7 +13,7 @@ export class SignupComponent implements OnInit {
 
   signupForm!: FormGroup;
 
-  constructor(private authService: AuthService){
+  constructor(private authService: AuthService, private router: Router){
   }
 
   ngOnInit(): void{
@@ -34,6 +35,7 @@ export class SignupComponent implements OnInit {
 
   signup(): void{
     this.authService.signup(this.signupForm.value).subscribe((msg) => console.log(msg));
+    this.router.navigate(["login"]);
     
   }
 }
