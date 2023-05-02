820de99f2ac1ba1683d59656ada954f4752e5211
Isaack <121324944+Icy1989@users.noreply.github.com>
2023-05-02 13:08:07 +0800

    Add files via upload

--- OPE-app2.0/frontend/src/app/app.module.ts
+++ OPE-app2.0/frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,9 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { FormsModule } from '@angular/forms';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
@@ -32,11 +35,14 @@ import { SavedevalComponent } from './components/savedeval/savedeval.component';
     SavedevalComponent,
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
     MatButtonModule,
     MatCardModule,
     MatIconModule,
--- OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
+++ OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addprod',
   templateUrl: './addprod.component.html',
   styleUrls: ['./addprod.component.css']
 })
-export class AddprodComponent {
+export class AddprodComponent implements OnInit{
+  reviewText: string = "";
+  evaluatedText: string = "";
+  showResult: boolean = false;
 
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  submitReview() {
+    // Perform evaluation logic here
+    this.evaluatedText = this.reviewText + " has been evaluated!";
+    this.showResult = true;
+  }
 }
