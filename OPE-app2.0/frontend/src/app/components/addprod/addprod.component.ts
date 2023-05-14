43b9e0d3fadfd0bdd236bda1406687f5e41c66c7
martinStudent <202010803@feualabang.edu.ph>
2023-05-14 16:33:22 +0800

    +Added another features in addprod for Sentiment Classification via another form (output: sentence, sentiment)

--- OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
+++ OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
@@ -9,7 +9,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AddprodComponent implements OnInit {
   reviewText: string = "";
   evaluatedText: string = "";
+  sentimentText: string = "";
   showResult: boolean = false;
+  showResult2: boolean = false;
+  input2: string = "";
+  sentimentClass: string = "";
 
   constructor(private http: HttpClient) {}
 
@@ -17,10 +21,15 @@ export class AddprodComponent implements OnInit {
 
   submitReview() {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    const data = { reviewText: this.reviewText };
-    this.http.post('http://localhost:5000/data', data, { headers }).subscribe({
+    // const data = { reviewText: this.reviewText };
+    const data = { "sentences": [
+      "The battery life on this device is impressive.",
+      "The camera takes stunning photos in low light.",
+      "The screen quality is excellent with vibrant colors."
+  ] };
+    this.http.post('http://localhost:5000/extract-aspects', data, { headers }).subscribe({
       next: (response: any) => {
-        console.log('Response from Flask app:', response);
+        console.log('Response from Flask app:', JSON.stringify(data));
       },
       error: (error: any) => {
         console.error('Error occurred:', error);
@@ -29,5 +38,39 @@ export class AddprodComponent implements OnInit {
         console.log('Request completed');
       }
     });
+
+    fetch('http://localhost:5000/aspects').then(response => {
+      response.json().then(data => {
+        this.evaluatedText = JSON.stringify(data);
+        console.log(data);
+        this.showResult = true;
+      });
+    });
+  }
+
+  submitForm2() {
+    console.log("submitForm2 " + this.input2);
+    let data = { "sentence": this.input2 };
+    
+    let options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  
+    fetch('http://localhost:5000/sentiment', options)
+      .then(response => response.json())
+      .then(data => {
+        const [sentence, sentiment] = data[0];
+        console.log(data);
+        // Handle response here
+        this.sentimentText = "Sentence: " + sentence;
+        this.sentimentClass = " Sentiment: " + sentiment;
+        this.showResult2 = true;
+      })
+      .catch(error => {
+        console.error(error);
+        // Handle error here
+      });
   }
 }
