d210c53d66257ea68854216cdae494fddbc360ae
Isaack <121324944+Icy1989@users.noreply.github.com>
2023-05-16 21:48:11 +0800

    Add files via upload

--- OPE-app2.0/frontend/src/app/components/savedeval/savedeval.component.ts
+++ OPE-app2.0/frontend/src/app/components/savedeval/savedeval.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-savedeval',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./savedeval.component.css']
 })
 export class SavedevalComponent {
+
+  constructor(private authService: AuthService, private router: Router) { }
 
 }
