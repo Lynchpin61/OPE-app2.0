2857a5bc00aff87cb40a682477a87f6ec131d935
Isaack Panganiban <isaackpanganiban02@gmail.com>
2023-06-03 23:44:21 +0800

    addprod design

--- OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
+++ OPE-app2.0/frontend/src/app/components/addprod/addprod.component.ts
@@ -1,27 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
-
 @Component({
   selector: 'app-addprod',
   templateUrl: './addprod.component.html',
   styleUrls: ['./addprod.component.css']
 })
-export class AddprodComponent implements OnInit{
+export class AddprodComponent implements OnInit {
   reviewText: string = "";
   evaluatedText: string = "";
   showResult: boolean = false;
+  images: string[] = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Lazada_%282019%29.svg/800px-Lazada_%282019%29.svg.png?20190626132505f",
+    "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" 
+  ];
+  currentImageIndex: number = 0;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
-    
+    setInterval(() => {
+      this.changeImage();
+    }, 10000);
+  }
+
+  changeImage() {
+    const imageElement = document.getElementById("image") as HTMLImageElement;
+    if (imageElement) {
+      imageElement.classList.add("fade-out");
+      setTimeout(() => {
+        imageElement.onload = () => {
+          imageElement.classList.add("fade-in");
+          imageElement.classList.remove("fade-out");
+        };
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+        imageElement.src = this.images[this.currentImageIndex];
+      }, 500);
+    }
   }
 
   submitReview() {
     // Perform evaluation logic here
     this.evaluatedText = this.reviewText + " has been evaluated!";
     this.showResult = true;
-    this.router.navigate(['prodeval'])
+    this.router.navigate(['prodeval']);
   }
 }
