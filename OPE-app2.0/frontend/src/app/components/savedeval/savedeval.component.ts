20125a2c87da85384ed001297128df1d03353afc
Isaack Panganiban <isaackpanganiban02@gmail.com>
2023-06-04 01:31:36 +0800

    savedeval

--- OPE-app2.0/frontend/src/app/components/savedeval/savedeval.component.ts
+++ OPE-app2.0/frontend/src/app/components/savedeval/savedeval.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AuthService } from "src/app/services/auth.service";
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-savedeval',
   templateUrl: './savedeval.component.html',
@@ -10,6 +9,21 @@ import { Router } from '@angular/router';
 })
 export class SavedevalComponent {
 
+  products: any[] = [
+    { id: 1, name: 'Earphone', link: 'https://www.lazada.com/' },
+  ];
+
   constructor(private authService: AuthService, private router: Router) { }
 
+  viewEvaluation(productId: number) {
+    // Logic to view evaluation based on product ID
+  }
+
+  printEvaluation(productId: number) {
+    // Logic to print evaluation based on product ID
+  }
+
+  deleteProduct(productId: number) {
+    // Logic to delete product based on product ID
+  }
 }
