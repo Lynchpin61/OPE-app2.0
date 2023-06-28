d4bd8c3956baea1d4e0b611ad42f28174fc73475
Lynchpin61 <131472200+Lynchpin61@users.noreply.github.com>
2023-06-28 20:51:54 +0800

    Add files via upload

---
+++ OPE-app2.0/frontend/src/app/services/email.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  private url = "http://localhost:3000/email";
+  constructor(private httpreq:HttpClient) { }
+
+  sendMessage(body: any){
+    let headers = {
+      headers : new HttpHeaders({
+        'Content-Type' :'application/json'
+      })
+    }
+    return this.httpreq.post(`${this.url}`,body,headers);
+  }
+}
